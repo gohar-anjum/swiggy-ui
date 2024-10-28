99736bf1a81cc6fd769b3d5263dcde287b812aa9
gohar-anjum <goharanjum49@gmail.com>
2024-10-28 23:10:10 +0500

    Working on Header with layer and sidebar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../index.css';
 import { RiArrowDropDownLine } from "react-icons/ri";
+
 function Header() {
-  return (
+    const [toggle, setToggle] = useState(false);
+    const showSideMenu = () =>{
+        setToggle(true);
+    }
+    const hideSideMenu = ()=>{
+        setToggle(false);
+    }
+    return (
     <>
+        <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} 
+            style={{
+                opacity: toggle ? 1 : 0,   
+                visibility: toggle ? "visible" : "hidden"
+            }}>
+            <div onClick={
+                (e)=>{
+                    e.stopPropagation();
+                }} className='w-[500px] bg-white h-full absolute duration-[400ms]'
+            style={{
+                left: toggle ? '0%' : '-100%' 
+            }}>
+            </div>
+        </div>
         <header className='p-[10px] shadow-xl'>
             <div className='max-w-[1200px] mx-auto flex items-center'>
                 <div className='w-[45px]'><img src="images/logo.png" className='w-full' alt="" /></div>
                 <div className='pl-[40px] text-[#C0C1C7]'>
                     <span className='border-b-[2px] font-bold border-[black] text-[black]'>Old Delhi</span> 6, Old Delhi, Delhi, 110006, India
-                    <RiArrowDropDownLine fontSize={30} className='inline text-[#F0851D] cursor-pointer'/>
+                    <RiArrowDropDownLine onClick={showSideMenu} fontSize={30} className='inline text-[#F0851D] cursor-pointer'/>
                 </div>
             </div>
         </header>
@@ -16,4 +39,4 @@ function Header() {
   )
 }
 
-export default Header
+export default Header;
